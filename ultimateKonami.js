f1a014f1f88dbef5afeb3ad0a2e542de0a1b06c9
Robert <rdtheriault@yahoo.com>
2017-01-21 16:39:30 -0800

    Update ultimateKonami.js

--- ultimateKonami.js
+++ ultimateKonami.js
@@ -1,6 +1,10 @@
 /* Compilation of many awesome JavaScript/jQuery animations that are now activated by the Konami Code.
-Special thanks to everyone who wrote these amazing scripts (I tried to leave their notes in there).
-All I did was modify them to activate after the Konami Code was entered. */
+Special thanks to everyone who wrote these amazing scripts (I tried to leave their notes/credits in there).
+All I did was modify them to activate after the Konami Code was entered. 
+Most of the $ for jQuery were turned to jQuery so these should work on WordPress sites (unless otherwise noted)*/
+
+
+
 
 
 /*
@@ -10,7 +14,7 @@ INSTRUCTIONS
 
 You need the following three lines to run the code
 ```
-    var easter_egg = new Konami();
+  var easter_egg = new Konami();
 	easter_egg.code = function() { functionGoesHere(); }
 	easter_egg.load();
 ```
@@ -20,7 +24,10 @@ this will listen for the Komami Code
 Change the function to whooosh();
 
 #Hide everything
-CHange the function to hideFun();
+Change the function to hideFun();
+
+#Make it "rain"
+
 
 */
 
@@ -28,12 +35,193 @@ CHange the function to hideFun();
 
 
 
+////START "SHOW PIC"////
+
+////END "SHOW PIC"////
+
+////START "FOLLOW MOUSE"////
+
+////END "FOLLOW MOUSE"////
+
+////START "MAKE IT RAIN"////
+function rain(item) {
+
+	var picture = "http://www.sagefirellc.com/images/Leaf.png";
 
 
+	if (item === "leaf") {picture = "http://www.sagefirellc.com/images/leaf.png";}
+	else if (item === "snow") {picture = "http://www.sagefirellc.com/images/flake.png";}
+	else if (item === "jack") {picture = "http://www.sagefirellc.com/images/jack.png";}
+	else if (item === "turkey") {picture = "http://www.sagefirellc.com/images/turkey.png";}
+	else if (item === "ornamate") {picture = "http://www.sagefirellc.com/images/ornamate.png";}
+	else if (item === "rain") {picture = "http://www.sagefirellc.com/images/flake.png";}//{picture = "http://www.sagefirellc.com/images/rain.png";}
+	else {picture = "http://www.sagefirellc.com/images/flake.png";}
+
+    // How many frames per second?  More => smoother but more processor intensive
+    var fps = 30;
+
+    // How often should flakes be added?  Every 10 frames by default.  Greater => fewer flakes
+    var addNewFlakesEveryNFrames = 10;
+
+    // How many flakes should be added each time?  Greater => more flakes, more processor intensive
+    var newFlakesToAdd = 3;
+
+    // Controls the speed; 0.7 provides a nice speed
+    var speedControl = 0.7;
+
+    // -------------------------------
+    // -------------------------------
+    // -------------------------------
+    
+    // Holder variables
+    var flakes = [];
+    var additionCounter = 0;
+
+    // The flake creator function
+    function createFlake(curX, curY) {
+
+        // How unique should each flake be?  These values specify max unique speed and wiggle/drift
+        var maxSpeedOffset = 2;
+        var maxWiggleOffset = 10;
+
+        // How big should the flakes be?
+        var minSize = 25;
+        var maxSize = 55;
+
+        // How much drifting/wiggling should be allowed in the downward path?
+        var minWiggle = 10;
+        var maxWiggle = 40;
+
+        var sizePercent = Math.random();
+        var size = Math.floor(sizePercent * (maxSize - minSize) + minSize);
+        var opacity = 0.3 + Math.random() * 0.7;
+        var color = '#9CF';
+
+        // Create a unique speed offset, so each flake falls at a unique rate
+        var speedOffset = Math.floor(Math.random() * maxSpeedOffset);
+
+        // Create a unique wiggle amount based on size (bigger = more wiggle/drift)
+        var wiggle = minWiggle + (maxWiggleOffset * Math.random()) + (maxWiggle - minWiggle) * sizePercent;
+
+        var flake = jQuery('<div>').text(' ').css({
+            position: 'fixed',
+            left: curX,
+            top: curY,
+            zIndex: 1000000,
+            //opacity: opacity,
+            width: size,
+            height: size,
+            backgroundImage: "url('"+picture+"')",
+            backgroundSize: 'contain'
+        }).appendTo('body');
+        
+
+        var flakeObj = {
+            size: size,
+            sizePercent: sizePercent,
+            homeX: curX,
+            curX: curX,
+            curY: curY,
+            flake: flake,
+            uniqueSpeedOffset: speedOffset,
+            wiggle: wiggle,
+            wiggleCounter: Math.floor(100 * Math.random())
+        };
+        flakes.push(flakeObj);
+
+        return flakeObj;
+    }
+
+    // Create the sin table to save processing power later
+    var sinTable = [];
+    for (var i = 0; i < 100; i++) {
+        var sin = Math.sin((i / 100) * Math.PI * 2);
+        sinTable.push(sin);
+    }
+
+    // Track where the mouse is, so we can move flakes away from it
+    var mouseX = -200;
+    var mouseY = -200;
+    var $w = jQuery(window);
+    jQuery(document).mousemove(function(e) {
+        mouseX = e.pageX - $w.scrollLeft();
+        mouseY = e.pageY - $w.scrollTop();
+    });
 
 
+    function onEnterFrame() {
+        // Update existing flakes
+        var winH = $w.height();
+        for (var i = flakes.length - 1; i > -1; i--) {
+            var flakeObj = flakes[i];
+            var flake = flakeObj.flake;
+            var speed = 2 + (1 - flakeObj.sizePercent) * 5 + flakeObj.uniqueSpeedOffset; // bigger = slower to fall
+            speed *= speedControl; // apply the speed control
+            var curY = flakeObj.curY;
+            var newY = curY + speed;
 
-////START HIDE FUNNY
+            var wiggleCounter = flakeObj.wiggleCounter = (++flakeObj.wiggleCounter % 100);
+            var sin = sinTable[wiggleCounter];
+            var wiggle = flakeObj.wiggle * sin;
+            var newX = flakeObj.homeX + wiggle;
+
+            // If we are close to the mouse, force out of the way
+            var mouseXDist = Math.abs(mouseX - newX);
+            var mouseYDist = Math.abs(mouseY - newY);
+            var influenceArea = 150;
+            if (mouseXDist < influenceArea && mouseYDist < influenceArea) {
+                var maxForce = 10;
+                var dist = Math.sqrt(mouseXDist * mouseXDist + mouseYDist * mouseYDist);
+                if (dist < influenceArea) {
+                    var influence = maxForce * (1 - (dist / influenceArea));
+                    if (mouseY > newY) {
+                        newY -= influence;
+                        if (mouseX < newX) flakeObj.homeX += influence;
+                        else flakeObj.homeX -= influence;
+                    }
+                    else newY += influence;
+                }
+            }
+
+            flakeObj.curY = newY;
+            flakeObj.curX = newX;
+            flake.css({
+                top: newY,
+                left: newX
+            });
+
+            // Destroy flake if it has gone out of view by 100
+            if (newY > winH + 100) {
+                flake.remove();
+                flakeObj.flake = null;
+                flakes.splice(i, 1);
+            }
+        }
+
+        // Add any new flakes
+        if (++additionCounter % addNewFlakesEveryNFrames == 0) {
+            additionCounter = 0;
+
+            var minX = -100;
+            var maxX = jQuery(window).width() + 100;
+            var homeY = -100;
+            for (var i = 0; i < newFlakesToAdd; i++) {
+                var curX = minX + Math.floor(Math.random() * (maxX - minX));
+                var curY = homeY + Math.floor(100 * Math.random());
+                createFlake(curX, curY);
+            }
+        }
+    }
+
+    // Start the animation based on the requested frames per second
+    setInterval(onEnterFrame, 1000 / fps);
+
+}
+////END "MAKE IT RAIN"////
+
+
+////START "HIDE FUNNY"////
+//SageFire LLC code
 function hideFun() {
 	var divs = document.getElementsByTagName("body");
 	for(var i = 0; i < divs.length; i++){
@@ -49,9 +237,9 @@ function showFun() {
 	   divs[i].style.visibility = "visible";
 	}
 }
-////END HIDE FUNNY
+////END "HIDE FUNNY"////
 
-////START SHOOTING STAR
+////START "SHOOTING STAR"////
 var speed=25; // how fast - smaller is faster
 var how_often=2; // average time between re-appearances of a comet (in seconds)
 var how_many=15; // maximum number of comets in flight
@@ -181,9 +369,9 @@ function set_width() {
   swide=sw_min;
   shigh=sh_min;
 }
-////END SHOOTING STAR
+////END "SHOOTING STAR"////
 
-////START KONAMI CODE
+////START "KONAMI CODE"////
 /*
  * Konami-JS ~ 
  * :: Now with support for touch events and multiple instances for 
@@ -289,4 +477,4 @@ var Konami = function (callback) {
 
 	return konami;
 };
-////END KONAMI CODE
+////END "KONAMI CODE"////
